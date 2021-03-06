c0656a46a6ef1b34af532dddf46b5bb5c35bae22
Dave Clay <david.clay@walmart.com>
2021-03-06 11:22:36 -0800

    Add Table components

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,28 @@
-const Table = ({type}) => (
+import { connect } from "react-redux";
+import {
+} from "../redux/actions"
+
+const Table = ({table}) => (
     <div className="draggable table">
         <div className="table-content">
-            <span className="table-id"></span>
+            <span className="table-id">{table.id}</span>
             <div className="form-content">
-                <form className="form-inline" role="form">
+                <div className="form-inline" role="form">
                     <div className="form-group">
                         <input className="tableId" type="text" placeholder="10"/>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </div>
 )
+
+const mapStateToProps = (state, ownProps) => ({
+    ...ownProps,
+})
+
+export default connect(
+    mapStateToProps,
+    {
+    }
+)(Table);
--- src/components/TableLayout.js
+++ src/components/TableLayout.js
@@ -1,11 +1,14 @@
 import { connect } from "react-redux";
 import {
 } from "../redux/actions"
+import Table from "./Table"
 
 const TableLayout = ({tables}) => (
     <div id="container">
         {
-
+            tables.map(table => {
+                return <Table key={table.id} table={table}/>
+            })
         }
     </div>
 )
@@ -14,4 +17,8 @@ const mapStateToProps = state => ({
     tables: state.tables
 })
 
-export default TableLayout
+export default connect(
+    mapStateToProps,
+    {
+    }
+)(TableLayout);
